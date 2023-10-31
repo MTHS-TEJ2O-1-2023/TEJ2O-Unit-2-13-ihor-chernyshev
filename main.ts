1d3075e0daeff10da0812c430c363ced93832478
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-31 13:00:15 -0400

    python version

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Ihor Chernyshev
+ * Created on: Oct 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let neopixelStrip: neopixel.Strip = null
+let loopCounter = 4
+
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+input.onButtonPressed(Button.A, function() {
+  while (loopCounter >= 4){
+    neopixelStrip.setPixelColor(loopCounter, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    loopCounter = loopCounter - 1
+  }
+})
